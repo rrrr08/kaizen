5fc1bb1cee3203b2866e93e695c43fe66d62f629
rushangbagada <rushang697@gmail.com>
2025-12-28 22:09:42 +0530

    domian change for cloudinary

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: '**.cloudinary.com',
       },
     ],
   },
